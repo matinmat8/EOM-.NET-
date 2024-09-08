f05d65b98ae1fcbf8bfdacb33872b56c7d960f30
matinmat8 <matinmat8.mat@gmail.com>
2024-09-08 13:49:01 +0330

    Exception to not adding headers for uploading file

--- client-app/src/app/http-interceptor/http.interceptor.ts
+++ client-app/src/app/http-interceptor/http.interceptor.ts
@@ -6,6 +6,12 @@ import { Observable } from 'rxjs';
 export class HttpInterceptorService implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (req.url.includes('/song/add') && req.method === 'POST') {
+      const clonedRequest = req.clone({
+        headers: req.headers.delete('Content-Type')
+      });
+      return next.handle(clonedRequest);
+    }
     const modifiedReq = req.clone({
       headers: req.headers
         .set('Content-Type', 'application/json')
