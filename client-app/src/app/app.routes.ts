53b9a0f7282ad93e3b36620eabd64c02a643a3ae
matinmat8 <matinmat8.mat@gmail.com>
2024-09-09 23:42:45 +0330

    Changing the song route

--- client-app/src/app/app.routes.ts
+++ client-app/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { GenreAddComponentComponent } from './genre/genre-add-component/genre-add-component.component';
 import { AddAlbumComponent } from './album/add-album/add-album.component';
 import { SongReadComponent } from './song/song-read/song-read.component';
 import { SongDetailComponent } from './song/song-detail/song-detail.component';
@@ -10,14 +9,17 @@ import { SingerReadComponent } from './singer/singer-read/singer-read.component'
 import { SingerDetailComponent } from './singer/singer-detail/singer-detail.component';
 import { SingerDeleteComponent } from './singer/singer-delete/singer-delete.component';
 import { SingerUpdateComponent } from './singer/singer-update/singer-update.component';
+import { GenreCreateComponent } from './genre/genre-create/genre-create.component';
+import { SongUpdateComponent } from './song/song-update/song-update.component';
 
 
 export const routes: Routes = [
-    { path: 'songs', component:SongReadComponent },
-    { path: 'genre', component: GenreAddComponentComponent },
+    { path: '', component:SongReadComponent },
+    { path: 'genre', component: GenreCreateComponent },
     { path: 'album', component: AddAlbumComponent},
-    { path: 'songs/:id', component: SongDetailComponent },
+    { path: 'song/:id', component: SongDetailComponent },
     { path: 'song/add', component: SongCreateComponent },
+    { path: 'song/update/:id', component: SongUpdateComponent },
     { path: 'song/delete/:id', component: SongDeleteComponent },
     { path: 'singer/add', component:AddSingerComponent},
     { path: 'singer/list', component:SingerReadComponent },
