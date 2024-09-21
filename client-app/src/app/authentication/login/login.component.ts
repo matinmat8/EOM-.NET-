57dcbd70f5fa5626b20f940933eb071f2c38b286
matinmat8 <matinmat8.mat@gmail.com>
2024-09-21 23:50:07 +0330

    Handling Login Token

--- client-app/src/app/authentication/login/login.component.ts
+++ client-app/src/app/authentication/login/login.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../auth.service';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { error } from 'node:console';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +16,11 @@ import { error } from 'node:console';
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+    ) {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
@@ -26,10 +29,15 @@ export class LoginComponent {
 
   onSubmit() {
     if (this.loginForm.valid) {
-      this.authService.login(this.loginForm.value).subscribe(response => {
-        const token = response[0];
-        localStorage.setItem('token', token);
-        console.log("token", token)
+      this.authService.login(this.loginForm.value).subscribe((response : any) => {
+        const token = response.u[0];
+        try{
+          console.log(response);
+          localStorage.setItem('token', token);
+          this.router.navigate(['']);
+        } catch (e) {
+          console.error(e);
+        }
       });
     }
   }
